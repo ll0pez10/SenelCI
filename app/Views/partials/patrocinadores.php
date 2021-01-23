c7bb6b2e6c2017bf3e5e8da77a5b775eb5465cca
luan lopes <luanlopez7@gmail.com>
2021-01-23 09:40:02 -0300

    update

--- app/Views/partials/patrocinadores.php
+++ app/Views/partials/patrocinadores.php
@@ -1,9 +1,24 @@
-<?php 
-echo "<pre>";
-print_r ($patrocinadores);
-echo "</pre>";
-?>
 <div class= "row">
     <h3>Patrocinadores</h3>
-    <p>lorem lorem lorem</p>
+    <div class="row">
+        <?php foreach($patrocinadores as $sponsor):?>
+            <div class="col-xs-1-12">
+              <div class="card">
+                <div class="card-body">
+                  <img src="<?php [$sponsor['logo']]?>"  class = "card-img-top" alt="">
+                  <h3 class="card-title"><?=$sponsor['nome']?></h3>
+                  <?php if($sponsor['categoria'] == "Platina"):?>
+                      <button type="button" class="btn btn-primary"></button>
+                  <?php elseif($sponsor['categoria'] == "Ouro"):?>
+                      <button type="button" class="btn btn-warning"></button>
+                  <?php elseif($sponsor['categoria'] == "Prata"):?>
+                      <button type="button" class="btn btn-secondary"></button>
+                  <?php else:?>
+                      <button type="button" class="btn btn-dark"></button>
+                  <?php endif; ?>
+                </div>
+              </div>
+            </div>
+          <?php endforeach;?>
+    </div>
 </div>
