76c748b092075a0528ba273689714d071352ddbf
luan lopes <luanlopez7@gmail.com>
2021-05-02 22:45:17 -0300

    up

--- app/Views/components/sponsor.php
+++ app/Views/components/sponsor.php
@@ -2,8 +2,10 @@
     <h3>Patrocínio</h3>
     <h5>Se interessou?</h5>
     <p>
-        Saiba como a sua empresa pode participar desse evento e quais os
+        Saiba como a sua empresa pode participar desse evento e quais os <br>
         principais benefícios para nossos patrocinadores.
     </p>
-    <button class="btn btn-outline-dark">Saiba mais</button>
+    <br>
+    <br>
+    <button class="btn btn-outline-light">Saiba mais</button>
 </div>
