1bbf9a52d3e191e81a25092ceac8d1296f86d3ae
luan lopes <luanlopez7@gmail.com>
2021-05-02 22:22:01 -0300

    up

--- app/Controllers/Edicoes.php
+++ app/Controllers/Edicoes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Controller;
 class Edicoes extends BaseController
 {
 	public function edicao(){
-		#["ano" => $ano, "id"=> $id]
+		//["ano" => $ano, "id"=> $id]
 		/*["arvores_plantadas"=>'1050']*/
 		return view('paginas/senel');
 	}
